d39c1734c73a82f0b7da1687e03b3d991010edc8
Gary Lo <logap@me.com>
2022-09-27 15:39:32 +0800

    add lottie web worker test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
+import Head from 'next/head'
+import React from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>lottie web worker test</title>
+      </Head>
+      {/* FIXME: using lottie.min.js cannot run `lottie.useWebWorker(true)` */}
+      {/*<Script src="https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.9.6/lottie.min.js" strategy='beforeInteractive'/>*/}
+      <Script src='https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.9.6/lottie.js' strategy='beforeInteractive' />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
