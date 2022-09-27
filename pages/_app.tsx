8902d16caccba01977799f53b502c846297632a6
Gary Lo <logap@me.com>
2022-09-27 16:31:14 +0800

    switch to min version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,8 +11,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>lottie web worker test</title>
       </Head>
       {/* FIXME: using lottie.min.js cannot run `lottie.useWebWorker(true)` */}
-      {/*<Script src="https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.9.6/lottie.min.js" strategy='beforeInteractive'/>*/}
-      <Script src='https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.9.6/lottie.js' strategy='beforeInteractive' />
+      <Script
+        src='https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.9.6/lottie.min.js'
+        strategy='beforeInteractive'
+      />
+      {/*<Script src='https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.9.6/lottie.js' strategy='beforeInteractive' />*/}
       <Component {...pageProps} />
     </>
   )
